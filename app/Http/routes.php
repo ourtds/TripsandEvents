8bb6fd4a6647136d078c1e4fea218be603734b45
waleedtfps <waleed_tfps@hotmail.com>
2015-04-25 18:17:12 +0500

    netbeans changes again

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,6 +12,3 @@
 */
 
 Route::get('/', 'HomeController@index');
-Route::get('/login', 'LoginController@index');
-Route::get('/register', 'RegisterController@index');
-Route::post('/register', 'RegisterController@doregistration');
