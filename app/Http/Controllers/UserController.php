5a3be459eac7eda4b141cafdfabec2b3f4add052
mjiskinng <mannan.official@gmail.com>
2015-04-25 18:37:57 +0500

    MJ_1

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php namespace App\Http\Controllers;
+use Illuminate\Routing\Controller;
+class UserController extends Controller {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller renders your application's "dashboard" for users that
+	| are authenticated. Of course, you are free to change or remove the
+	| controller as you wish. It is just here to get your app started!
+	|
+	*/
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	
+
+	/**
+	 * Show the application dashboard to the user.
+	 *
+	 * @return Response
+	 */
+	public function register()
+	{
+                          
+	}
+        
+                    public function registeruser()
+	{
+                        
+	}
+        
+                    public function login()
+	{
+                          
+	}
+        
+                    public function loginuser()
+	{
+                          
+	}
+
+}
